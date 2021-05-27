3f77974325228489e5dd2c1f5970e95056a1539c
Valentin BOISGERAULT <valentinbf94@gmail.com>
2021-05-27 15:45:35 +0200

    Saut modifié + run

---
+++ Assets/Scripts 1/JumpDouble.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JumpDouble : MonoBehaviour
+{
+
+    public Rigidbody2D rb;
+    private Vector3 velocity = Vector3.zero;
+
+    public float jumpForce;
+    public bool isJumping;
+    //public bool isHitting;
+
+    void Start()
+    {
+
+    }
+
+    /*void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Solides")
+        {
+            isHitting = true;
+        }
+
+        Debug.Log("Collisions avec " + collision.gameObject.name);
+    }
+    */
+
+    void Update()
+    {
+
+        if (Input.GetButtonDown("Jump") /*&& isHitting == true*/) ;
+        {
+            isJumping = true;
+        }
+
+        if (isJumping == true)
+        {
+            rb.AddForce(new Vector2(0f, jumpForce));
+            /*isJumping = false;*/
+        }
+
+    }
+}
--- Assets/Scripts 1/PlayerMovement.cs
+++ Assets/Scripts 1/PlayerMovement.cs
@@ -4,43 +4,36 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-
-    public Transform GroundedLeft;
-    public Transform GroundedRight;
-
     public Rigidbody2D rb;
     private Vector3 velocity = Vector3.zero;
 
-    public float jumpForce;
-    public bool isJumping;
-    public bool isGrounded;
-
-    void Update()
-    {
-        isGrounded = Physics2D.OverlapArea(GroundedLeft.position, GroundedRight.position);
-
-        if(Input.GetButtonDown("Jump") && isGrounded == true)
-        {
-            isJumping = true;
-        }
-    }
-
+    public float Speed;
     public float moveSpeed;
+    public float RunSpeed;
+    public bool isRunning;
+
 
     void FixedUpdate()
     {
         float horizontalMovement = Input.GetAxis("Horizontal") * moveSpeed * Time.deltaTime;
         MovePlayer(horizontalMovement);
+
+        if (Input.GetKey(KeyCode.LeftShift))
+        {
+            isRunning = true;
+            moveSpeed = RunSpeed;
+        }
+
+        else
+        {
+            isRunning = false;
+            moveSpeed = Speed;
+        }
     }
 
     void MovePlayer(float _horizontalMovement)
     {
         Vector3 targetVelocity = new Vector2(_horizontalMovement, rb.velocity.y);
         rb.velocity = Vector3.SmoothDamp(rb.velocity, targetVelocity, ref velocity, .05f);
-
-        if (isJumping == true)
-        {
-            rb.AddForce(new Vector2(0f, jumpForce));
-        }
     }
 }
